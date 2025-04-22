4a3191100e3bfc9ab8e4cc50d1d5530cc53a1e44
AlessioNuzzo <alessionuzzo2@gmail.com>
2025-04-22 13:07:16 -0500

    Add files via upload

---
+++ StudentRegistrationSystemGUI/src/EE333/SRS_ClassLevel.java
@@ -0,0 +1,19 @@
+
+package EE333;
+
+/**
+ *
+ * @author aless
+ */
+public enum SRS_ClassLevel {
+    Undergraduate,
+    Graduate,
+    Instructor,
+    Other;
+    
+    public static SRS_ClassLevel ParseClassLevel( String SRS_ClassLevel ){
+        
+        return(Other);
+    }
+    
+}
