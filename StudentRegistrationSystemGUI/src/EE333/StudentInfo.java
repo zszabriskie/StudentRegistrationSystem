cd6caf879c62859669738c6d1cacf863b0fda417
Cfjohnst <150366793+Cfjohnst@users.noreply.github.com>
2025-04-25 01:46:23 -0500

    Add files via upload

---
+++ StudentRegistrationSystemGUI/src/EE333/StudentInfo.java
@@ -0,0 +1,85 @@
+package EE333;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cf_jo
+ */
+
+import java.util.Random;
+
+public class StudentInfo {
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String ID;
+    private UserLevel Level;
+
+    // Constructor
+    public StudentInfo(String FirstName, String LastName, String Email, UserLevel Level) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Email = Email;
+        this.ID = generateID();  
+        this.Level = Level;
+    }
+
+    // Method to generate a random ID
+    private String generateID() {
+        Random random = new Random();
+        String ID = "B0";
+        for (int i = 0; i < 7; i++) {
+            ID += random.nextInt(10); 
+        }
+        return ID;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    // Setters
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setLevel(UserLevel Level) {
+        this.Level = Level;
+    }
+
+    public String toStringStudent() {
+        return "Name: " + FirstName + " " + LastName + " ID: " + ID + " Email: " + Email;
+    }
+
+    public String toStringFullname() {
+        return FirstName + " " + LastName;
+    }
+}
+
+
+
---
+++ StudentRegistrationSystemGUI/src/EE333/UserLevel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package EE333;
+
+/**
+ *
+ * @author cf_jo
+ */
+public enum UserLevel {
+    Undergraduate,
+    Graduate,
+    Instructor,
+    Other
+}
